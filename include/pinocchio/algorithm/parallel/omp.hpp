2637496265944e1f3d760ac4009367d52c558359
Justin Carpentier <justin.carpentier@inria.fr>
2024-06-12 10:30:07 +0200

    parallel: deprecate file

--- include/pinocchio/algorithm/parallel/omp.hpp
+++ include/pinocchio/algorithm/parallel/omp.hpp
@@ -5,6 +5,8 @@
 #ifndef __pinocchio_algorithm_parallel_omp_hpp__
 #define __pinocchio_algorithm_parallel_omp_hpp__
 
+#include "pinocchio/macros.hpp"
+PINOCCHIO_PRAGMA_DEPRECATED_HEADER(pinocchio/algorithm/parallel/omp.hpp,pinocchio/utils/openmp.hpp)
 #include "pinocchio/utils/openmp.hpp"
 
 #endif // ifndef __pinocchio_algorithm_parallel_omp_hpp__
