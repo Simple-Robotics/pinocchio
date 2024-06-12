110c2ffa4ed4bee2e25de34aee0d91b71f4d8724
Justin Carpentier <justin.carpentier@inria.fr>
2024-06-12 10:21:54 +0200

    openmp: add OpenMPException

--- include/pinocchio/utils/openmp.hpp
+++ include/pinocchio/utils/openmp.hpp
@@ -1,11 +1,14 @@
 //
-// Copyright (c) 2021 INRIA
+// Copyright (c) 2021-2024 INRIA
 //
 
 #ifndef __pinocchio_utils_openmp_hpp__
 #define __pinocchio_utils_openmp_hpp__
 
 #include <cstdlib>
+#include <exception>
+#include <mutex>
+#include <omp.h>
 
 namespace pinocchio
 {
@@ -22,6 +25,66 @@ namespace pinocchio
 
     return num_threads;
   }
+  
+  struct OpenMPException
+  {
+    explicit OpenMPException(const bool throw_on_deletion = false)
+    : m_exception_ptr(nullptr)
+    , m_throw_on_deletion(throw_on_deletion)
+    {}
+    
+    void rethrowException() const
+    {
+      if(this->m_exception_ptr)
+        std::rethrow_exception(this->m_exception_ptr);
+    }
+    
+    std::exception_ptr getException() const
+    {
+      return m_exception_ptr;
+    }
+    
+    bool hasThrown() const
+    {
+      return this->m_exception_ptr != nullptr;
+    }
+    
+    template <typename Function, typename... Parameters>
+    void run(Function f, Parameters... params)
+    {
+      try
+      {
+        f(params...);
+      }
+      catch (...)
+      {
+        this->captureException();
+      }
+    }
+    
+    void captureException() 
+    {
+      std::unique_lock<std::mutex> guard(this->m_lock);
+      this->m_exception_ptr = std::current_exception();
+    }
+    
+    void reset()
+    {
+      this->m_exception_ptr = nullptr;
+    }
+    
+    ~OpenMPException()
+    {
+      if(m_throw_on_deletion)
+        this->rethrowException();
+    }
+    
+  protected:
+    
+    std::exception_ptr m_exception_ptr;
+    std::mutex         m_lock;
+    const bool         m_throw_on_deletion;
+  }; // struct OpenMPException
 }
 
 #endif // ifndef __pinocchio_utils_openmp_hpp__
