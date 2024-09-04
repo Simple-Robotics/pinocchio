8824e32f6aca751e4f92a7fa2914c9ebb4b20e89
Justin Carpentier <justin.carpentier@inria.fr>
2024-09-04 18:20:09 +0200

    core: add missing explicit keyword

--- include/pinocchio/algorithm/contact-info.hpp
+++ include/pinocchio/algorithm/contact-info.hpp
@@ -66,7 +66,7 @@ namespace pinocchio
     typedef _Scalar Scalar;
     typedef Eigen::Matrix<Scalar, -1, 1, Eigen::ColMajor, 6> Vector6Max;
 
-    BaumgarteCorrectorParametersTpl(int size = 6)
+    explicit BaumgarteCorrectorParametersTpl(int size = 6)
     : Kp(size)
     , Kd(size)
     {
