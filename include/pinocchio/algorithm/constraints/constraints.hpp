824a5d4d209ef25cecd1727c569b4e5ee52ebb9b
Justin Carpentier <justin.carpentier@inria.fr>
2024-09-09 18:00:10 +0200

    algo/constraints: add include headers

--- include/pinocchio/algorithm/constraints/constraints.hpp
+++ include/pinocchio/algorithm/constraints/constraints.hpp
@@ -1,14 +1,13 @@
 //
-// Copyright (c) 2023 INRIA
+// Copyright (c) 2023-2024 INRIA
 //
 
 #ifndef __pinocchio_algorithm_constraints_constraints_hpp__
 #define __pinocchio_algorithm_constraints_constraints_hpp__
 
-#include "pinocchio/algorithm/constraints/fwd.hpp"
-
-namespace pinocchio
-{
-}
+#include "pinocchio/algorithm/constraints/constraint-model-generic.hpp"
+#include "pinocchio/algorithm/constraints/constraint-data-generic.hpp"
+#include "pinocchio/algorithm/contact-info.hpp"
+#include "pinocchio/algorithm/constraints/frictional-joint-constraint.hpp"
 
 #endif // ifndef __pinocchio_algorithm_constraints_constraints_hpp__
