36f9ba08ec6b46331e3a17a1d264065f5218a4b0
quentinll <quentin.le-lidec@inria.fr>
2024-09-23 14:09:00 +0200

    admm solver: removing unused line and changing relative convergence criteria

--- include/pinocchio/algorithm/admm-solver.hxx
+++ include/pinocchio/algorithm/admm-solver.hxx
@@ -227,7 +227,6 @@ namespace pinocchio
       // y-update
       //      rhs *= alpha;
       //      rhs += (1-alpha)*y_previous;
-      rhs = x_;
       rhs -= z_ / (tau * rho);
       computeConeProjection(cones, rhs, y_);
       //      std::cout << "y_: " << y_.transpose() << std::endl;
@@ -260,6 +259,7 @@ namespace pinocchio
         VectorXs & dx = rhs;
         dx = x_ - x_previous;
         dual_feasibility_vector.noalias() += mu_prox * dx;
+        proximal_metric = std::max(dx.template lpNorm<Eigen::Infinity>(), proximal_metric);
       }
 
       //      delassus.applyOnTheRight(x_,dual_feasibility_vector);
