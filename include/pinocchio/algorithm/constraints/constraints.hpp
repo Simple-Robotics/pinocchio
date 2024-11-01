434774e7bd9a5423f00b50ec19a2f5f9160b82bb
Justin Carpentier <justin.carpentier@inria.fr>
2024-11-01 18:01:47 +0100

    algo/constraints: add further include

--- include/pinocchio/algorithm/constraints/constraints.hpp
+++ include/pinocchio/algorithm/constraints/constraints.hpp
@@ -8,11 +8,14 @@
 #include "pinocchio/algorithm/constraints/fwd.hpp"
 #include "pinocchio/algorithm/contact-info.hpp"
 #include "pinocchio/algorithm/constraints/frictional-joint-constraint.hpp"
+#include "pinocchio/algorithm/constraints/frictional-point-constraint.hpp"
+#include "pinocchio/algorithm/constraints/bilateral-point-constraint.hpp"
 
 #include "pinocchio/algorithm/constraints/constraint-model-generic.hpp"
 #include "pinocchio/algorithm/constraints/constraint-data-generic.hpp"
 
 #include "pinocchio/algorithm/constraints/box-set.hpp"
+#include "pinocchio/algorithm/constraints/unbounded-set.hpp"
 #include "pinocchio/algorithm/constraints/coulomb-friction-cone.hpp"
 
 #endif // ifndef __pinocchio_algorithm_constraints_constraints_hpp__
