02cf42bb4cd3cb67215c3f5b2eb7b5ec3c4c718d
Justin Carpentier <justin.carpentier@inria.fr>
2024-11-19 09:41:39 +0100

    constraints: fix genericity of constraint utils

--- include/pinocchio/algorithm/contact-solver-utils.hpp
+++ include/pinocchio/algorithm/contact-solver-utils.hpp
@@ -18,136 +18,141 @@ namespace pinocchio
 
     /// \brief Project a vector x on the vector of cones.
     template<
-      typename Scalar,
+      typename ConstraintSet,
       typename ConstraintAllocator,
       typename VectorLikeIn,
       typename VectorLikeOut>
     void computeConeProjection(
-      const std::vector<CoulombFrictionConeTpl<Scalar>, ConstraintAllocator> & cones,
+      const std::vector<ConstraintSet, ConstraintAllocator> & sets,
       const Eigen::DenseBase<VectorLikeIn> & x,
       const Eigen::DenseBase<VectorLikeOut> & x_proj_)
     {
       assert(x.size() == x_proj_.size());
       Eigen::DenseIndex index = 0;
       VectorLikeOut & x_proj = x_proj_.const_cast_derived();
-      for (const auto & cone : cones)
+      for (const auto & set : sets)
       {
-        x_proj.template segment<3>(index) = cone.project(x.template segment<3>(index));
-        assert(cone.isInside(x_proj.template segment<3>(index), Scalar(1e-12)));
-        index += 3;
+        const auto size = set.size();
+        x_proj.segment(index, size) = set.project(x.segment(index, size));
+        index += size;
       }
     }
 
     /// \brief Project a vector x on the dual of the cones contained in the vector of cones.
     template<
-      typename Scalar,
+      typename ConstraintSet,
       typename ConstraintAllocator,
       typename VectorLikeIn,
       typename VectorLikeOut>
     void computeDualConeProjection(
-      const std::vector<CoulombFrictionConeTpl<Scalar>, ConstraintAllocator> & cones,
+      const std::vector<ConstraintSet, ConstraintAllocator> & sets,
       const Eigen::DenseBase<VectorLikeIn> & x,
       const Eigen::DenseBase<VectorLikeOut> & x_proj_)
     {
       assert(x.size() == x_proj_.size());
-      Eigen::DenseIndex index = 0;
       VectorLikeOut & x_proj = x_proj_.const_cast_derived();
-      for (const auto & cone : cones)
+      Eigen::DenseIndex index = 0;
+      for (const auto & set : sets)
       {
-        x_proj.template segment<3>(index) = cone.dual().project(x.template segment<3>(index));
-        assert(cone.dual().isInside(x_proj.template segment<3>(index), Scalar(1e-12)));
-        index += 3;
+        const auto size = set.size();
+        x_proj.segment(index, size) = set.dual().project(x.segment(index, size));
+        //        assert(cone.dual().isInside(x_proj.template segment<3>(index), Scalar(1e-12)));
+        index += size;
       }
     }
 
     template<
-      typename Scalar,
+      typename ConstraintSet,
       typename ConstraintAllocator,
       typename VectorLikeVelocity,
       typename VectorLikeForce>
-    Scalar computeConicComplementarity(
-      const std::vector<CoulombFrictionConeTpl<Scalar>, ConstraintAllocator> & cones,
+    typename ConstraintSet::Scalar computeConicComplementarity(
+      const std::vector<ConstraintSet, ConstraintAllocator> & sets,
       const Eigen::DenseBase<VectorLikeVelocity> & velocities,
       const Eigen::DenseBase<VectorLikeForce> & forces)
     {
+      typedef typename ConstraintSet::Scalar Scalar;
       assert(velocities.size() == forces.size());
       Eigen::DenseIndex index = 0;
-      Scalar complementarity = 0;
-      for (const auto & cone : cones)
+      Scalar complementarity = Scalar(0);
+      for (const auto & set : sets)
       {
-        const Scalar cone_complementarity = cone.computeConicComplementarity(
-          velocities.template segment<3>(index), forces.template segment<3>(index));
+        const auto size = set.size();
+        const Scalar cone_complementarity = set.computeConicComplementarity(
+          velocities.segment(index, size), forces.segment(index, size));
         complementarity = math::max(complementarity, cone_complementarity);
-        index += 3;
+        index += size;
       }
 
       return complementarity;
     }
 
     template<
-      typename Scalar,
+      typename ConstraintSet,
       typename ConstraintAllocator,
       typename VectorLikeIn,
       typename VectorLikeOut>
     void computeComplementarityShift(
-      const std::vector<CoulombFrictionConeTpl<Scalar>, ConstraintAllocator> & cones,
+      const std::vector<ConstraintSet, ConstraintAllocator> & sets,
       const Eigen::DenseBase<VectorLikeIn> & velocities,
       const Eigen::DenseBase<VectorLikeOut> & shift_)
     {
       assert(velocities.size() == shift_.size());
       Eigen::DenseIndex index = 0;
       VectorLikeOut & shift = shift_.const_cast_derived();
-      for (const auto & cone : cones)
+      for (const auto & set : sets)
       {
-        shift.template segment<3>(index) =
-          cone.computeNormalCorrection(velocities.template segment<3>(index));
-        index += 3;
+        const auto size = set.size();
+        shift.segment(index, size) = set.computeNormalCorrection(velocities.segment(index, size));
+        index += size;
       }
     }
 
-    template<typename Scalar, typename ConstraintAllocator, typename VectorLikeIn>
-    Scalar computePrimalFeasibility(
-      const std::vector<CoulombFrictionConeTpl<Scalar>, ConstraintAllocator> & cones,
+    template<typename ConstraintSet, typename ConstraintAllocator, typename VectorLikeIn>
+    typename ConstraintSet::Scalar computePrimalFeasibility(
+      const std::vector<ConstraintSet, ConstraintAllocator> & sets,
       const Eigen::DenseBase<VectorLikeIn> & forces)
     {
-      typedef CoulombFrictionConeTpl<Scalar> Cone;
-      typedef typename Cone::Vector3 Vector3;
+      typedef typename ConstraintSet::Vector3 Vector3;
+      typedef typename ConstraintSet::Scalar Scalar;
 
       Eigen::DenseIndex index = 0;
       Scalar norm(0);
-      for (const auto & cone : cones)
+      for (const auto & set : sets)
       {
-        const Vector3 df_projected =
-          cone.project(forces.template segment<3>(index)) - forces.template segment<3>(index);
+        const auto size = set.size();
+        Vector3 df_projected =
+          set.project(forces.segment(index, size)) - forces.segment(index, size);
         norm = math::max(norm, df_projected.norm());
-        index += 3;
+        index += size;
       }
 
       return norm;
     }
 
     template<
-      typename Scalar,
+      typename ConstraintSet,
       typename ConstraintAllocator,
       typename ForceVector,
       typename VelocityVector>
-    Scalar computeReprojectionError(
-      const std::vector<CoulombFrictionConeTpl<Scalar>, ConstraintAllocator> & cones,
+    typename ConstraintSet::Scalar computeReprojectionError(
+      const std::vector<ConstraintSet, ConstraintAllocator> & sets,
       const Eigen::DenseBase<ForceVector> & forces,
       const Eigen::DenseBase<VelocityVector> & velocities)
     {
-      typedef CoulombFrictionConeTpl<Scalar> Cone;
-      typedef typename Cone::Vector3 Vector3;
+      typedef typename ConstraintSet::Vector3 Vector3;
+      typedef typename ConstraintSet::Scalar Scalar;
 
       Eigen::DenseIndex index = 0;
       Scalar norm(0);
-      for (const auto & cone : cones)
+      for (const auto & set : sets)
       {
+        const auto size = set.size();
         const Vector3 df_projected =
-          forces.template segment<3>(index)
-          - cone.project(forces.template segment<3>(index) - velocities.template segment<3>(index));
+          forces.segment(index, size)
+          - set.project(forces.segment(index, size) - velocities.segment(index, size));
         norm = math::max(norm, df_projected.norm());
-        index += 3;
+        index += size;
       }
 
       return norm;
