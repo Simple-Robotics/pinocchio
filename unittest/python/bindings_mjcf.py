4f210d23bc026f150bf6bfe6f052a25e1897bcff
quentinll <quentin.le-lidec@inria.fr>
2024-11-29 19:23:13 +0100

    parser xml: adding test on bindings

---
+++ unittest/python/bindings_mjcf.py
@@ -0,0 +1,19 @@
+import unittest
+from pathlib import Path
+import pinocchio as pin
+
+
+class TestMJCFBindings(unittest.TestCase):
+    def test_load(self):
+        model = pin.Model()
+        current_dir = Path(__file__).parent
+        model_dir = current_dir / "../models/"
+        model_path = model_dir / "closed_chain.xml"
+        constraint_models = pin.StdVec_BilateralPointConstraintModel()
+        model, constraint_models = pin.buildModelFromMJCF(
+            model_path, model, constraint_models
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
