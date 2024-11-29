c5fb3589f5a23c52f5a4a645f821d3c4ba85b7cf
quentinll <quentin.le-lidec@inria.fr>
2024-11-29 12:04:46 +0100

    admm: adding test on composite joint limit

--- unittest/admm-solver.cpp
+++ unittest/admm-solver.cpp
@@ -629,4 +629,123 @@ BOOST_AUTO_TEST_CASE(joint_limit_freeflyer)
   }
 }
 
+BOOST_AUTO_TEST_CASE(joint_limit_composite)
+{
+  // We test limits for a joint with nq>1
+  JointModelComposite joint;
+  joint.addJoint(JointModelRX());
+  joint.addJoint(JointModelRY());
+  Model model;
+  model.addJoint(0, JointModelFreeFlyer(), SE3::Identity(), "composite");
+  model.lowerPositionLimit = Eigen::VectorXd::Ones(model.nq) * -10000;
+  model.lowerPositionLimit[1] = 0;
+  model.upperPositionLimit = Eigen::VectorXd::Ones(model.nq) * 10000;
+
+  const SE3::Vector3 box_dims = SE3::Vector3::Ones();
+  const double box_mass = 10;
+  const Inertia box_inertia = Inertia::FromBox(box_mass, box_dims[0], box_dims[1], box_dims[2]);
+
+  model.appendBodyToJoint(1, box_inertia);
+  model.gravity.setZero();
+  Data data(model);
+
+  Eigen::VectorXd q0 = Eigen::VectorXd::Zero(model.nq);
+  const Eigen::VectorXd v0 = Eigen::VectorXd::Zero(model.nv);
+  const Eigen::VectorXd tau_push_against_lower_bound = -Eigen::VectorXd::Ones(model.nv);
+
+  const double dt = 1e-3;
+
+  typedef JointLimitConstraintModel ConstraintModel;
+  typedef ConstraintModel::ConstraintData ConstraintData;
+  std::vector<ConstraintModel> constraint_models;
+  std::vector<ConstraintData> constraint_datas;
+
+  ConstraintModel joint_limit_constraint_model(model, ConstraintModel::JointIndexVector(1, 1));
+  constraint_models.push_back(joint_limit_constraint_model);
+
+  for (const auto & cm : constraint_models)
+    constraint_datas.push_back(cm.createData());
+
+  const Eigen::VectorXd v_free_against_lower_bound =
+    dt * aba(model, data, q0, v0, tau_push_against_lower_bound, Convention::WORLD);
+  const Eigen::VectorXd v_free_move_away =
+    dt * aba(model, data, q0, v0, -tau_push_against_lower_bound, Convention::WORLD);
+
+  // Cholesky of the Delassus matrix
+  crba(model, data, q0, Convention::WORLD);
+  ContactCholeskyDecomposition chol(model, constraint_models);
+  chol.compute(model, data, constraint_models, constraint_datas, 1e-10);
+
+  auto G_expression = chol.getDelassusCholeskyExpression();
+  const auto G_plain = G_expression.matrix();
+  const Eigen::MatrixXd delassus_matrix_plain = G_expression.matrix();
+
+  Eigen::MatrixXd constraint_jacobian(joint_limit_constraint_model.size(), model.nv);
+  constraint_jacobian.setZero();
+  getConstraintsJacobian(model, data, constraint_models, constraint_datas, constraint_jacobian);
+
+  // Gravity pushes the freeflyer against the lower bound
+  {
+    data.q_in = q0;
+    const auto & cmodel = constraint_models[0];
+    auto & cdata = constraint_datas[0];
+    cmodel.calc(model, data, cdata);
+
+    const Eigen::VectorXd g_against_lower_bound = constraint_jacobian * v_free_against_lower_bound;
+    const Eigen::VectorXd g_tilde_against_lower_bound =
+      g_against_lower_bound + cdata.constraint_residual / dt;
+
+    Eigen::VectorXd constraint_velocity = Eigen::VectorXd::Zero(cmodel.size());
+    Eigen::VectorXd primal_solution = Eigen::VectorXd::Zero(cmodel.size());
+    ADMMContactSolver admm_solver(int(delassus_matrix_plain.rows()));
+    admm_solver.setAbsolutePrecision(1e-10);
+    admm_solver.setRelativePrecision(1e-14);
+    const bool has_converged = admm_solver.solve(
+      G_expression, g_tilde_against_lower_bound, constraint_models, primal_solution);
+    primal_solution = admm_solver.getPrimalSolution();
+    BOOST_CHECK(has_converged);
+
+    constraint_velocity = G_plain * primal_solution + g_against_lower_bound;
+    Eigen::VectorXd dual_solution = admm_solver.getDualSolution();
+
+    BOOST_CHECK(std::fabs(primal_solution.dot(dual_solution)) <= 1e-8);
+    BOOST_CHECK(std::abs(constraint_velocity(1)) < 1e-6);
+    BOOST_CHECK(
+      (dual_solution - (G_plain * primal_solution + g_tilde_against_lower_bound)).isZero(1e-6));
+
+    BOOST_CHECK(
+      std::abs(
+        (tau_push_against_lower_bound + constraint_jacobian.transpose() * primal_solution / dt)(1))
+      < 1e-6);
+  }
+
+  // External torques compensate the gravity to push the freeflyer away from the lower bound
+  {
+    data.q_in = q0;
+    const auto & cmodel = constraint_models[0];
+    auto & cdata = constraint_datas[0];
+    cmodel.calc(model, data, cdata);
+
+    const Eigen::VectorXd g_move_away = constraint_jacobian * v_free_move_away;
+    const Eigen::VectorXd g_tilde_move_away = g_move_away + cdata.constraint_residual / dt;
+
+    Eigen::VectorXd dual_solution = Eigen::VectorXd::Zero(cmodel.size());
+    Eigen::VectorXd primal_solution = Eigen::VectorXd::Zero(cmodel.size());
+    ADMMContactSolver admm_solver(int(delassus_matrix_plain.rows()));
+    admm_solver.setAbsolutePrecision(1e-10);
+    admm_solver.setRelativePrecision(1e-14);
+    const bool has_converged =
+      admm_solver.solve(G_expression, g_tilde_move_away, constraint_models, primal_solution);
+    primal_solution = admm_solver.getPrimalSolution();
+    BOOST_CHECK(has_converged);
+
+    dual_solution = G_plain * primal_solution + g_move_away;
+    Eigen::VectorXd dual_solution2 = admm_solver.getDualSolution();
+
+    BOOST_CHECK(std::fabs(primal_solution.dot(dual_solution)) <= 1e-8);
+    BOOST_CHECK(primal_solution.isZero());
+    BOOST_CHECK(dual_solution.isApprox(g_move_away));
+  }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
