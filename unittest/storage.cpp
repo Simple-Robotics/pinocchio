708a80ae6eeca35acafeab6ea303662573dcb0d8
Justin Carpentier <justin.carpentier@inria.fr>
2024-12-13 09:39:43 +0100

    test/storage: refactor code

--- unittest/storage.cpp
+++ unittest/storage.cpp
@@ -9,39 +9,36 @@
 #include <boost/utility/binary.hpp>
 
 using namespace pinocchio;
-typedef EigenStorageTpl<double> EigenStorage;
+typedef EigenStorageTpl<Eigen::MatrixXd> EigenStorageMatrix;
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-BOOST_AUTO_TEST_CASE(eigen_storage)
+BOOST_AUTO_TEST_CASE(eigen_storage_matrix)
 {
   const Eigen::DenseIndex rows = 10, cols = 20;
 
   const Eigen::DenseIndex initial_capacity = rows * cols;
-  EigenStorage storage(initial_capacity);
+  EigenStorageMatrix storage(rows, cols);
 
   BOOST_CHECK(storage.capacity() == initial_capacity);
+  BOOST_CHECK(storage.rows() == rows);
+  BOOST_CHECK(storage.cols() == cols);
 
-  auto matrix_map = storage.as<Eigen::MatrixXd>(rows, cols);
+  EigenStorageMatrix::RefMapType matrix_map = storage.map();
   BOOST_CHECK(matrix_map.data() == storage.data());
 
   matrix_map.setIdentity();
-  BOOST_CHECK(storage.as<Eigen::MatrixXd>(rows, cols).isIdentity(0.));
-  BOOST_CHECK(
-    static_cast<const EigenStorage &>(storage).as<Eigen::MatrixXd>(rows, cols).isIdentity(0.));
+  BOOST_CHECK(storage.map().isIdentity(0.));
+  BOOST_CHECK(static_cast<const EigenStorageMatrix &>(storage).map().isIdentity(0.));
   matrix_map.setOnes();
-  BOOST_CHECK(storage.as<Eigen::VectorXd>(initial_capacity).isOnes(0.));
-  BOOST_CHECK(
-    static_cast<const EigenStorage &>(storage).as<Eigen::VectorXd>(initial_capacity).isOnes(0.));
+  BOOST_CHECK(storage.map().isOnes(0.));
+  BOOST_CHECK(static_cast<const EigenStorageMatrix &>(storage).map().isOnes(0.));
 
   // Check resize
-  storage.conservativeResize(2 * storage.capacity());
-  BOOST_CHECK(storage.as<Eigen::MatrixXd>(rows, cols).isOnes(0.));
-  auto matrix_map2 = storage.as<Eigen::MatrixXd>(rows, 2 * cols);
-  BOOST_CHECK(matrix_map2.leftCols(cols).isOnes(0.));
-
-  // Check throw
-  BOOST_CHECK_THROW(storage.as<Eigen::MatrixXd>(4 * rows, 4 * cols), std::invalid_argument);
+  const Eigen::DenseIndex new_rows = 2 * rows, new_cols = cols;
+  storage.conservativeResize(new_rows, new_cols);
+  BOOST_CHECK(matrix_map.data() == storage.data());
+  BOOST_CHECK(storage.map().topLeftCorner(rows, cols).isOnes(0.));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
