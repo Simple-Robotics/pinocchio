9e2e2e0751ae48a4bd4439500b9e50b4e10e774c
Justin Carpentier <justin.carpentier@inria.fr>
2024-12-20 17:52:21 +0100

    algo/constraints: continue refactoring of contactInverseDynamics

--- include/pinocchio/algorithm/contact-inverse-dynamics.hpp
+++ include/pinocchio/algorithm/contact-inverse-dynamics.hpp
@@ -9,7 +9,6 @@
 #include "pinocchio/algorithm/contact-jacobian.hpp"
 #include "pinocchio/algorithm/proximal.hpp"
 
-// #include <cstdlib>
 #include <boost/optional.hpp>
 
 namespace pinocchio
@@ -62,8 +61,10 @@ namespace pinocchio
     const boost::optional<VectorLikeImp> & lambda_guess = boost::none)
   {
     PINOCCHIO_UNUSED_VARIABLE(model);
-    using VectorXs = Eigen::Matrix<Scalar, Eigen::Dynamic, 1, Options>;
-    using Vector3 = Eigen::Matrix<Scalar, 3, 1, Options>;
+    typedef ModelTpl<Scalar, Options, JointCollectionTpl> Model;
+    using VectorXs = typename Model::VectorXs;
+    using Vector3 = typename Model::Vector3;
+    typedef FrictionalPointConstraintModelTpl<Scalar, Options> ConstraintModel;
 
     const Eigen::Index problem_size = R.size();
     const std::size_t n_contacts = contact_models.size();
@@ -72,8 +73,8 @@ namespace pinocchio
     PINOCCHIO_CHECK_ARGUMENT_SIZE(contact_datas.size(), n_contacts);
     PINOCCHIO_CHECK_INPUT_ARGUMENT(
       check_expression_if_real<Scalar>(settings.mu > Scalar(0)), "mu has to be strictly positive");
-    VectorXs R_prox; // TODO: malloc
-    R_prox = R + VectorXs::Constant(problem_size, settings.mu);
+
+    const VectorXs R_prox = R + VectorXs::Constant(problem_size, settings.mu);
     if (lambda_guess)
     {
       data.lambda_c = lambda_guess.get();
@@ -83,6 +84,7 @@ namespace pinocchio
     {
       data.lambda_c.setZero();
     }
+
     Scalar lambda_c_prev_norm_inf = data.lambda_c.template lpNorm<Eigen::Infinity>();
     bool abs_prec_reached = false, rel_prec_reached = false;
     settings.iter = 1;
@@ -90,34 +92,39 @@ namespace pinocchio
     {
       settings.absolute_residual = Scalar(0);
       settings.relative_residual = Scalar(0);
-      for (std::size_t cone_id = 0; cone_id < n_contacts; ++cone_id)
+      Eigen::DenseIndex row_id = 0;
+      for (std::size_t constraint_id = 0; constraint_id < n_contacts; ++constraint_id)
       {
-        const auto row_id = (Eigen::Index)(3 * cone_id);
 
-        const FrictionalPointConstraintModelTpl<Scalar, Options> & cmodel = contact_models[cone_id];
+        const ConstraintModel & cmodel = contact_models[constraint_id];
+        const auto constraint_size = cmodel.size();
+
         const CoulombFrictionCone & cone = cmodel.set();
-        Vector3 lambda_c_prev = data.lambda_c.template segment<3>(row_id);
+        const Vector3 lambda_c_previous = data.lambda_c.template segment<3>(row_id);
         auto lambda_segment = data.lambda_c.template segment<3>(row_id);
-        auto R_prox_segment = R_prox.template segment<3>(row_id);
-        auto c_ref_segment = c_ref.template segment<3>(row_id);
-        Vector3 sigma_segment =
+        const auto R_prox_segment = R_prox.template segment<3>(row_id);
+        const auto c_ref_segment = c_ref.template segment<3>(row_id);
+        const Vector3 sigma_segment =
           c_ref_segment + (R.template segment<3>(row_id).array() * lambda_segment.array()).matrix();
-        Vector3 desaxce_segment = cone.computeNormalCorrection(sigma_segment);
-        Vector3 c_cor_segment = c_ref_segment + desaxce_segment;
+        const Vector3 desaxce_correction = cone.computeNormalCorrection(sigma_segment);
+        const Vector3 c_cor_segment = c_ref_segment + desaxce_correction;
         lambda_segment =
-          -((c_cor_segment - settings.mu * lambda_c_prev).array() / R_prox_segment.array())
+          -((c_cor_segment - settings.mu * lambda_c_previous).array() / R_prox_segment.array())
              .matrix();
-        lambda_segment = cone.weightedProject(lambda_segment, R_prox_segment);
-        // evaluate convergence criteria
+        lambda_segment = cone.weightedProject(Vector3(lambda_segment), R_prox_segment);
+
+        // Compute convergence criteria
         Scalar contact_complementarity =
-          cone.computeConicComplementarity(sigma_segment + desaxce_segment, lambda_segment);
+          cone.computeConicComplementarity(sigma_segment + desaxce_correction, lambda_segment);
         Scalar dual_feasibility =
           std::abs(math::min(0., sigma_segment(2))); // proxy of dual feasibility
         settings.absolute_residual = math::max(
           settings.absolute_residual, math::max(contact_complementarity, dual_feasibility));
-        Vector3 dlambda_c = lambda_segment - lambda_c_prev;
+        const Vector3 dlambda_c = lambda_segment - lambda_c_previous;
         Scalar proximal_metric = dlambda_c.template lpNorm<Eigen::Infinity>();
         settings.relative_residual = math::max(settings.relative_residual, proximal_metric);
+
+        row_id += constraint_size;
       }
 
       const Scalar lambda_c_norm_inf = data.lambda_c.template lpNorm<Eigen::Infinity>();
@@ -140,6 +147,7 @@ namespace pinocchio
 
       lambda_c_prev_norm_inf = lambda_c_norm_inf;
     }
+
     return data.lambda_c;
   }
 
@@ -263,32 +271,34 @@ namespace pinocchio
     ProximalSettingsTpl<Scalar> & settings,
     const boost::optional<VectorLikeLam> & lambda_guess = boost::none)
   {
-    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, Options> MatrixXs;
-    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1, Options> VectorXs;
-    typedef ForceTpl<Scalar, Options> Force;
-    typedef FrictionalPointConstraintDataTpl<Scalar, Options> RigidConstraintData;
+    typedef ModelTpl<Scalar, Options, JointCollectionTpl> Model;
+    //    typedef FrictionalPointConstraintDataTpl<Scalar, Options> ConstraintData;
+
+    typedef typename Model::MatrixXs MatrixXs;
+    typedef typename Model::VectorXs VectorXs;
+    typedef typename Model::Force Force;
 
     const Eigen::Index problem_size = R.size();
     const std::size_t n_contacts = contact_models.size();
     MatrixXs J = MatrixXs::Zero(problem_size, model.nv); // TODO: malloc
     getConstraintsJacobian(model, data, contact_models, contact_datas, J);
     VectorXs v_ref, c_ref, tau_c;
-    v_ref = v + dt * a;
+    v_ref.noalias() = v + dt * a;
     c_ref.noalias() = J * v_ref; // TODO should rather use the displacement
     c_ref += constraint_correction;
     c_ref /= dt; // we work with a formulation on forces
     computeContactForces(
       model, data, c_ref, contact_models, contact_datas, R, settings, lambda_guess);
-    container::aligned_vector<Force> fext((std::size_t)(model.njoints));
-    for (std::size_t i = 0; i < (std::size_t)(model.njoints); i++)
-    {
-      fext[i] = Force::Zero();
-    }
+    PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(Force)
+    fext(std::size_t(model.njoints), Force::Zero());
+
+    Eigen::DenseIndex row_id = 0;
     for (std::size_t i = 0; i < n_contacts; i++)
     {
       const FrictionalPointConstraintModel & cmodel = contact_models[i];
-      const auto row_id = (Eigen::Index)(3 * i);
-      auto lambda_segment = data.lambda_c.template segment<3>(row_id);
+      const auto constraint_size = cmodel.size();
+
+      auto lambda_segment = data.lambda_c.segment(row_id, constraint_size);
       typename RigidConstraintData::Matrix6 actInv_transpose1 =
         cmodel.joint1_placement.toActionMatrixInverse();
       actInv_transpose1.transposeInPlace();
@@ -297,6 +307,8 @@ namespace pinocchio
         cmodel.joint2_placement.toActionMatrixInverse();
       actInv_transpose2.transposeInPlace();
       fext[cmodel.joint2_id] += Force(actInv_transpose2.template leftCols<3>() * lambda_segment);
+
+      row_id += constraint_size;
     }
     rnea(model, data, q, v, a, fext);
     return data.tau;
