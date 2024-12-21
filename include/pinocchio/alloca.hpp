f577cc0a40442a1899d7bee8c92fcdeb01ec37ba
Justin Carpentier <justin.carpentier@inria.fr>
2024-12-21 18:32:02 +0100

    core/alloca: add PINOCCHIO_EIGEN_MAP_ALLOCA_ALIGNED

--- include/pinocchio/alloca.hpp
+++ include/pinocchio/alloca.hpp
@@ -11,8 +11,15 @@
   #include <alloca.h>
 #endif
 
-#define PINOCCHIO_EIGEN_MAP_ALLOCA(Scalar, rows, cols)                                             \
-  static_cast<Scalar *>(alloca(size_t(rows * cols) * sizeof(Scalar))), rows, cols
 #define PINOCCHIO_ALLOCA EIGEN_ALLOCA
+#define PINOCCHIO_ALIGNED_PTR(ptr, align)                                                          \
+  reinterpret_cast<void *>(((intptr_t)ptr + (align - 1)) & ~(align - 1))
+#define PINOCCHIO_EIGEN_MAP_ALLOCA(Scalar, rows, cols)                                             \
+  PINOCCHIO_EIGEN_MAP_ALLOCA_ALIGNED(Scalar, rows, cols, EIGEN_DEFAULT_ALIGN_BYTES)
+#define PINOCCHIO_EIGEN_MAP_ALLOCA_ALIGNED(Scalar, rows, cols, align)                              \
+  static_cast<Scalar *>(PINOCCHIO_ALIGNED_PTR(                                                     \
+    PINOCCHIO_ALLOCA(size_t(rows * cols) * sizeof(Scalar) + (align > 0 ? (align - 1) : 1)),        \
+    align)),                                                                                       \
+    rows, cols
 
 #endif // ifndef __pinocchio_alloca_hpp__
