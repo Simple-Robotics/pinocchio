ec575c8a3a20c6f13a66843b2f881593d754adfa
Justin Carpentier <justin.carpentier@inria.fr>
2024-12-21 18:31:35 +0100

    core/alloca: introduce PINOCCHIO_ALLOCA

--- include/pinocchio/alloca.hpp
+++ include/pinocchio/alloca.hpp
@@ -13,5 +13,6 @@
 
 #define PINOCCHIO_EIGEN_MAP_ALLOCA(Scalar, rows, cols)                                             \
   static_cast<Scalar *>(alloca(size_t(rows * cols) * sizeof(Scalar))), rows, cols
+#define PINOCCHIO_ALLOCA EIGEN_ALLOCA
 
 #endif // ifndef __pinocchio_alloca_hpp__
