8c259854c873342f9ba5f57036695f980e0b4b88
Justin Carpentier <justin.carpentier@inria.fr>
2024-12-23 19:30:49 +0100

    test/constraints: refactor and test constrained inverse dynamics

--- unittest/contact-inverse-dynamics.cpp
+++ unittest/contact-inverse-dynamics.cpp
@@ -3,83 +3,149 @@
 //
 
 #include <iostream>
-#include "pinocchio/algorithm/aba.hpp"
-#include "pinocchio/algorithm/rnea.hpp"
-#include "pinocchio/algorithm/frames.hpp"
-#include "pinocchio/algorithm/jacobian.hpp"
-#include "pinocchio/algorithm/centroidal.hpp"
-#include "pinocchio/algorithm/kinematics.hpp"
-#include "pinocchio/algorithm/contact-info.hpp"
-#include "pinocchio/algorithm/compute-all-terms.hpp"
-#include "pinocchio/algorithm/constrained-dynamics.hpp"
-#include "pinocchio/algorithm/contact-dynamics.hpp"
+
 #include "pinocchio/algorithm/contact-inverse-dynamics.hpp"
 #include "pinocchio/algorithm/joint-configuration.hpp"
+
+#include "pinocchio/algorithm/constraints/constraints.hpp"
 #include "pinocchio/multibody/sample-models.hpp"
-#include "pinocchio/utils/timer.hpp"
-#include "pinocchio/spatial/classic-acceleration.hpp"
+#include "pinocchio/algorithm/contact-solver-utils.hpp"
 
 #include <boost/test/unit_test.hpp>
+
+using namespace Eigen;
+using namespace pinocchio;
+using namespace pinocchio::internal;
+
+typedef PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(FrictionalPointConstraintModel)
+  FrictionalPointConstraintModelVector;
+typedef PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(FrictionalPointConstraintData)
+  FrictionalPointConstraintDataVector;
+
+void init(Model & model, FrictionalPointConstraintModelVector & constraint_models)
+{
+  pinocchio::buildModels::humanoidRandom(model, true);
+  model.lowerPositionLimit.head<3>().fill(-1.);
+  model.upperPositionLimit.head<3>().fill(1.);
+
+  const std::string RF = "rleg6_joint";
+  FrictionalPointConstraintModel ci_RF(model, model.getJointId(RF));
+  ci_RF.set().mu = 0.4;
+  constraint_models.push_back(ci_RF);
+
+  const std::string LF = "lleg6_joint";
+  FrictionalPointConstraintModel ci_LF(model, model.getJointId(LF));
+  ci_LF.set().mu = 0.4;
+  constraint_models.push_back(ci_LF);
+}
+
+FrictionalPointConstraintDataVector
+createData(const FrictionalPointConstraintModelVector & constraint_models)
+{
+  FrictionalPointConstraintDataVector constraint_datas;
+
+  for (const auto & cmodel : constraint_models)
+  {
+    constraint_datas.push_back(cmodel.createData());
+  }
+
+  return constraint_datas;
+}
+
+template<typename VectorLike>
+typename PINOCCHIO_EIGEN_PLAIN_TYPE(VectorLike) abs(const Eigen::MatrixBase<VectorLike> & vec)
+{
+  return Eigen::abs(vec.array()).matrix();
+}
+
+template<typename VectorLike>
+void makeIsotropic(
+  FrictionalPointConstraintModelVector & constraint_models,
+  const Eigen::MatrixBase<VectorLike> & vec_)
+{
+  auto & vec = vec_.const_cast_derived();
+
+  Eigen::Index row_id = 0;
+  for (const auto & cmodel : constraint_models)
+  {
+    const auto csize = cmodel.size();
+
+    auto vec_seg = vec.segment(row_id, csize);
+    vec_seg[1] = vec_seg[0];
+
+    row_id += csize;
+  }
+}
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
 BOOST_AUTO_TEST_CASE(test_contact_inverse_dynamics_3D)
 {
-  using namespace Eigen;
-  using namespace pinocchio;
+#ifdef NDEBUG
+  const int num_tests = int(1e4);
+#else
+  const int num_tests = int(1e2);
+#endif
 
-  pinocchio::Model model;
-  pinocchio::buildModels::humanoidRandom(model, true);
-  pinocchio::Data data(model), data_ref(model);
+  Model model;
+  FrictionalPointConstraintModelVector constraint_models;
 
-  model.lowerPositionLimit.head<3>().fill(-1.);
-  model.upperPositionLimit.head<3>().fill(1.);
+  init(model, constraint_models);
+  const double mu_prox = 1e-4;
   VectorXd q = randomConfiguration(model);
-
   VectorXd v = VectorXd::Random(model.nv);
   VectorXd tau = VectorXd::Random(model.nv);
-
-  const std::string RF = "rleg6_joint";
-  //  const Model::JointIndex RF_id = model.getJointId(RF);
-  const std::string LF = "lleg6_joint";
-  //  const Model::JointIndex LF_id = model.getJointId(LF);
-
-  // Contact models and data
-  typedef PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(FrictionalPointConstraintModel)
-    FrictionalConstraintModelVector;
-  typedef PINOCCHIO_STD_VECTOR_WITH_EIGEN_ALLOCATOR(FrictionalPointConstraintData)
-    FrictionalConstraintDataVector;
-
-  FrictionalConstraintModelVector contact_models;
-  FrictionalConstraintDataVector contact_datas;
-  FrictionalPointConstraintModel ci_RF(model, model.getJointId(RF));
-  ci_RF.set().mu = 0.4;
-  contact_models.push_back(ci_RF);
-  contact_datas.push_back(FrictionalPointConstraintData(ci_RF));
-  FrictionalPointConstraintModel ci_LF(model, model.getJointId(LF));
-  ci_LF.set().mu = 0.4;
-  contact_models.push_back(ci_LF);
-  contact_datas.push_back(FrictionalPointConstraintData(ci_LF));
-
-  FrictionalConstraintDataVector contact_datas_ref(contact_datas);
+  VectorXd a = Eigen::VectorXd::Zero(model.nv);
 
   Eigen::DenseIndex constraint_dim = 0;
-  for (size_t k = 0; k < contact_models.size(); ++k)
-    constraint_dim += contact_models[k].size();
+  for (const auto & cmodel : constraint_models)
+  {
+    constraint_dim += cmodel.size();
+  }
 
-  Eigen::MatrixXd J_ref(constraint_dim, model.nv);
-  J_ref.setZero();
+  BOOST_CHECK(constraint_dim > 0);
 
-  double dt = 1e-3;
-  Eigen::VectorXd R = Eigen::VectorXd::Zero(constraint_dim);
-  Eigen::VectorXd constraint_correction = Eigen::VectorXd::Zero(constraint_dim);
-  boost::optional<Eigen::VectorXd> lambda_guess = boost::none;
-  Eigen::VectorXd a = Eigen::VectorXd::Zero(model.nv);
-  ProximalSettings prox_settings(1e-12, 1e-6, 1);
-  initConstraintDynamics(model, data_ref, contact_models);
-  contactInverseDynamics(
-    model, data_ref, q, v, a, dt, contact_models, contact_datas, R, constraint_correction,
-    prox_settings, lambda_guess);
+  for (int n = 0; n < num_tests; ++n)
+  {
+    Eigen::VectorXd R = abs(Eigen::VectorXd::Random(constraint_dim))
+                        + Eigen::VectorXd::Constant(constraint_dim, 1e-10);
+    makeIsotropic(constraint_models, R);
+
+    const Eigen::VectorXd R_inv = Eigen::inverse(R.array());
+    BOOST_CHECK(MatrixXd(R_inv.asDiagonal() * R.asDiagonal()).isIdentity());
+
+    ProximalSettings prox_settings(1e-12, 1e-12, /*mu = */ 0, 200);
+
+    const Eigen::VectorXd x_positive = abs(Eigen::VectorXd::Random(constraint_dim));
+    const Eigen::VectorXd x_in_cone = Eigen::VectorXd::Zero(constraint_dim);
+
+    computeConeProjection(constraint_models, x_positive, x_in_cone);
+
+    const Eigen::VectorXd constraint_velocity_ref = -(R.asDiagonal() * x_in_cone).eval();
+    const Eigen::VectorXd sigma_ref = (constraint_velocity_ref + R.asDiagonal() * x_in_cone);
+    BOOST_CHECK(sigma_ref.isZero());
+
+    Eigen::VectorXd x_sol = Eigen::VectorXd::Zero(constraint_dim);
+
+    bool has_converged = computeInverseDynamicsConstraintForces(
+      constraint_models, constraint_velocity_ref, R, x_sol, prox_settings);
+    BOOST_CHECK(has_converged);
+
+    Eigen::VectorXd sigma = constraint_velocity_ref + R.asDiagonal() * x_sol;
+
+    Eigen::VectorXd sigma_correction(sigma);
+    computeDeSaxeCorrection(constraint_models, sigma, sigma_correction);
+    sigma += sigma_correction;
+
+    BOOST_CHECK(sigma.isZero(1e-10));
+    Eigen::VectorXd sigma_projected(sigma);
+    computeDualConeProjection(constraint_models, sigma, sigma_projected);
+    BOOST_CHECK((sigma_projected - sigma).lpNorm<Eigen::Infinity>() <= 1e-10);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_contact_whole_body_inverse_dynamics_3D)
+{
 }
 
 BOOST_AUTO_TEST_SUITE_END()
