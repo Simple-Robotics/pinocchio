9dbf11415c78bc10d1a1fac19e6feeaffc4ce7d9
Justin Carpentier <justin.carpentier@inria.fr>
2025-01-17 19:02:39 +0100

    core: simplify typedef

--- include/pinocchio/multibody/data.hpp
+++ include/pinocchio/multibody/data.hpp
@@ -83,7 +83,7 @@ namespace pinocchio
     typedef Eigen::Matrix<Scalar, 3, 1, Options> Vector3;
     typedef Eigen::Matrix<Scalar, 6, 1, Options> Vector6;
 
-    typedef Eigen::Matrix<Scalar, 6, 1, Options> Vector6c;
+    typedef Vector6 Vector6c;
     typedef Eigen::Matrix<Scalar, 1, 6, Eigen::RowMajor | Options> Vector6r;
 
     /// \brief Dense vectorized version of a joint configuration vector.
