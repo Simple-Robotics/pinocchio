0dbd0604886b4539fb0a00cc33b713e1162f6e86
Yann de Mont-Marin <ydemontm@mac.home>
2025-01-31 22:33:37 +0100

    Expose all constraint and their vector

--- bindings/python/algorithm/constraints/expose-constraints.cpp
+++ bindings/python/algorithm/constraints/expose-constraints.cpp
@@ -15,14 +15,14 @@ namespace pinocchio
     {
       typedef context::ConstraintCollectionDefault::ConstraintModelVariant ConstraintModelVariant;
       boost::mpl::for_each<ConstraintModelVariant::types>(ConstraintModelExposer());
-      // boost::mpl::for_each<ConstraintModelVariant::types>(ConstraintStdExposer());
+      boost::mpl::for_each<ConstraintModelVariant::types>(ConstraintStdExposer());
       bp::to_python_converter<ConstraintModelVariant, ConstraintVariantVisitor<ConstraintModelVariant>>();
       ConstraintModelPythonVisitor<context::ConstraintModel>::expose();
       StdAlignedVectorPythonVisitor<context::ConstraintModel>::expose("StdVec_ConstraintModelVector");
 
       typedef context::ConstraintCollectionDefault::ConstraintDataVariant ConstraintDataVariant;
-      // boost::mpl::for_each<ConstraintDataVariant::types>(ConstraintDataExposer());
-      // boost::mpl::for_each<ConstraintModelVariant::types>(ConstraintStdExposer());
+      boost::mpl::for_each<ConstraintDataVariant::types>(ConstraintDataExposer());
+      boost::mpl::for_each<ConstraintModelVariant::types>(ConstraintStdExposer());
       bp::to_python_converter<ConstraintDataVariant, ConstraintVariantVisitor<ConstraintDataVariant>>();
       ConstraintDataPythonVisitor<context::ConstraintData>::expose();
       StdAlignedVectorPythonVisitor<context::ConstraintData>::expose("StdVec_ConstraintDataVector");
