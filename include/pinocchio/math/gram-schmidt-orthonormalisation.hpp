aff5fcef40bf93459c5af81ad4b35609bcdf184e
Justin Carpentier <justin.carpentier@inria.fr>
2025-02-02 20:19:35 +0100

    math: add orthonormalization

--- include/pinocchio/math/gram-schmidt-orthonormalisation.hpp
+++ include/pinocchio/math/gram-schmidt-orthonormalisation.hpp
@@ -38,8 +38,58 @@ namespace pinocchio
         vec -= alpha * col;
     }
 
-    if (threshold == 0)
-      assert((basis.transpose() * vec).isZero(1e-10));
+    //    std::cout << "basis.transpose() * vec: " << (basis.transpose() *
+    //    vec).eval().array().abs().maxCoeff() << std::endl; if(!(basis.transpose() *
+    //    vec).isZero(1e-10))
+    //    {
+    //      const auto res = (basis.transpose() * vec).eval();
+    //      for(Eigen::Index i = 0; i < res.size(); ++i)
+    //      {
+    //        std::cout << "i - " << i << " value: " << res[i] << std::endl;
+    //      }
+    //      std::cout << "vec: " << vec.norm() << std::endl;
+    //    }
+
+    //    if (threshold == 0)
+    //      assert((basis.transpose() * vec).isZero(1e-10));
+  }
+
+  ///  \brief Perform the orthonormalization of the input matrix via the Gram-Schmidt procedure.
+  ///
+  ///  \param[in,out] matrix_ Matrix to orthonormalize.
+  ///  \param[in] threshold Only perform the orthonormalization if the scalar product between the
+  /// current column and the input vector is above the given threshold.
+  ///
+  template<typename MatrixType>
+  void orthonormalization(
+    const Eigen::MatrixBase<MatrixType> & matrix_,
+    const typename MatrixType::Scalar & threshold = 0)
+  {
+    auto & matrix = matrix_.const_cast_derived();
+
+    for (Eigen::Index i = 0; i < matrix.cols(); ++i)
+    {
+      auto vec = matrix.col(i);
+      if (i > 0)
+        orthogonalization(matrix.leftCols(i), vec);
+
+      const auto vec_norm = vec.norm();
+      vec /= vec_norm;
+    }
+  }
+
+  ///  \brief Check whether the input matrix is orthonormal up to a given input precision.
+  ///
+  ///  \param[in,out] matrix_ Matrix to orthonormalize.
+  ///  \param[in] prec Numerical precision of the check (optional).
+  ///
+  template<typename MatrixType>
+  bool isOrthonormal(
+    const Eigen::MatrixBase<MatrixType> & matrix,
+    const typename MatrixType::Scalar & prec =
+      Eigen::NumTraits<typename MatrixType::Scalar>::dummy_precision())
+  {
+    return (matrix.transpose() * matrix).isIdentity(prec);
   }
 } // namespace pinocchio
 
