cc944097575d54dcf1506f51de1648698bbb8c8c
Louis Montaut <louismontaut@gmail.com>
2025-02-02 14:28:48 +0100

    constraints: fix missing set comparision in operator==

--- include/pinocchio/algorithm/constraints/weld-constraint.hpp
+++ include/pinocchio/algorithm/constraints/weld-constraint.hpp
@@ -204,7 +204,7 @@ namespace pinocchio
     ///
     bool operator==(const WeldConstraintModelTpl & other) const
     {
-      return base() == other.base();
+      return base() == other.base() && m_set == other.m_set;
     }
 
     ///
