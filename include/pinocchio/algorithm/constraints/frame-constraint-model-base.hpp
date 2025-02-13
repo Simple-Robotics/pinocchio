e724939823bf735319e4a3abcae33a6b597e6b22
Louis Montaut <louismontaut@gmail.com>
2025-02-13 19:49:09 +0100

    constraints/frame: use compliance from ConstraintModelCommonParameters

--- include/pinocchio/algorithm/constraints/frame-constraint-model-base.hpp
+++ include/pinocchio/algorithm/constraints/frame-constraint-model-base.hpp
@@ -11,7 +11,7 @@
 #include "pinocchio/spatial/skew.hpp"
 #include "pinocchio/algorithm/fwd.hpp"
 #include "pinocchio/algorithm/constraints/fwd.hpp"
-#include "pinocchio/algorithm/constraints/constraint-model-base.hpp"
+#include "pinocchio/algorithm/constraints/constraint-model-base-common-parameters.hpp"
 #include "pinocchio/algorithm/constraints/baumgarte-corrector-parameters.hpp"
 
 namespace pinocchio
@@ -52,7 +52,7 @@ namespace pinocchio
   ///  \brief Contact model structure containg all the info describing the rigid contact model
   ///
   template<typename Derived>
-  struct FrameConstraintModelBase : ConstraintModelBase<Derived>
+  struct FrameConstraintModelBase : ConstraintModelBaseCommonParameters<Derived>
   {
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -65,7 +65,7 @@ namespace pinocchio
     static const ConstraintFormulationLevel constraint_formulation_level =
       traits<FrameConstraintModelBase>::constraint_formulation_level;
 
-    typedef ConstraintModelBase<Derived> Base;
+    typedef ConstraintModelBaseCommonParameters<Derived> Base;
     typedef typename traits<Derived>::ConstraintModel ConstraintModel;
     typedef typename traits<Derived>::ConstraintData ConstraintData;
 
@@ -80,9 +80,6 @@ namespace pinocchio
     typedef Eigen::Matrix<Scalar, 3, 1, Options> Vector3;
     typedef Eigen::Matrix<Scalar, 6, 1, Options> Vector6;
     typedef Vector6 VectorConstraintSize;
-    typedef typename traits<Derived>::ComplianceVectorType ComplianceVectorType;
-    typedef typename traits<Derived>::ComplianceVectorTypeRef ComplianceVectorTypeRef;
-    typedef typename traits<Derived>::ComplianceVectorTypeConstRef ComplianceVectorTypeConstRef;
 
     Base & base()
     {
@@ -144,8 +141,7 @@ namespace pinocchio
     size_t depth_joint1, depth_joint2;
 
   protected:
-    /// \brief Compliance associated with the contact model
-    ComplianceVectorType m_compliance = ComplianceVectorType::Zero();
+    using Base::m_compliance;
 
   public:
     ///
@@ -301,18 +297,6 @@ namespace pinocchio
       return colwise_span_indexes;
     }
 
-    /// \brief Returns the compliance internally stored in the constraint model
-    ComplianceVectorTypeConstRef compliance() const
-    {
-      return m_compliance;
-    }
-
-    /// \brief Returns the compliance internally stored in the constraint model
-    ComplianceVectorTypeRef compliance()
-    {
-      return m_compliance;
-    }
-
     template<typename OtherDerived>
     friend struct FrameConstraintModelBase;
 
@@ -342,7 +326,7 @@ namespace pinocchio
              && depth_joint1 == other.depth_joint1 && depth_joint2 == other.depth_joint2
              && colwise_sparsity == other.colwise_sparsity
              && colwise_span_indexes == other.colwise_span_indexes
-             && loop_span_indexes == other.loop_span_indexes && m_compliance == other.m_compliance;
+             && loop_span_indexes == other.loop_span_indexes;
     }
 
     ///
@@ -782,7 +766,7 @@ namespace pinocchio
     template<typename NewScalar, typename OtherDerived>
     void cast(FrameConstraintModelBase<OtherDerived> & res) const
     {
-      res.base() = base();
+      Base::template cast<NewScalar>(res);
       res.joint1_id = joint1_id;
       res.joint2_id = joint2_id;
       res.joint1_placement = joint1_placement.template cast<NewScalar>();
@@ -802,8 +786,6 @@ namespace pinocchio
       res.depth_joint1 = depth_joint1;
       res.depth_joint2 = depth_joint2;
       res.loop_span_indexes = loop_span_indexes;
-      res.m_compliance = m_compliance.template cast<NewScalar>();
-      ;
     }
 
   protected:
@@ -892,6 +874,9 @@ namespace pinocchio
           loop_span_indexes.push_back(col_id);
         }
       }
+
+      // Set compliance
+      m_compliance = Vector6::Zero();
     }
   }; // FrameConstraintModelBase<Derived>
 
