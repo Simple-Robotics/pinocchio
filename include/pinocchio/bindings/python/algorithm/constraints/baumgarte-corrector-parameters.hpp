2fc3fbca26e0fb3a88a22f045397302f0c150830
Justin Carpentier <justin.carpentier@inria.fr>
2025-02-16 20:25:26 +0100

    python: restore BaumgarteCorrectorParametersPythonVisitor

---
+++ include/pinocchio/bindings/python/algorithm/constraints/baumgarte-corrector-parameters.hpp
@@ -0,0 +1,55 @@
+//
+// Copyright (c) 2025 INRIA
+//
+
+#ifndef __pinocchio_python_algorithm_constraints_baumgarte_corrector_parameters_hpp__
+#define __pinocchio_python_algorithm_constraints_baumgarte_corrector_parameters_hpp__
+
+#include "pinocchio/algorithm/constraints/baumgarte-corrector-parameters.hpp"
+
+#include "pinocchio/bindings/python/utils/cast.hpp"
+#include "pinocchio/bindings/python/utils/copyable.hpp"
+
+namespace pinocchio
+{
+  namespace python
+  {
+    namespace bp = boost::python;
+
+    template<typename BaumgarteCorrectorParameters>
+    struct BaumgarteCorrectorParametersPythonVisitor
+    : public boost::python::def_visitor<
+        BaumgarteCorrectorParametersPythonVisitor<BaumgarteCorrectorParameters>>
+    {
+      typedef typename BaumgarteCorrectorParameters::Scalar Scalar;
+      typedef typename BaumgarteCorrectorParameters::VectorType VectorType;
+      typedef BaumgarteCorrectorParameters Self;
+
+    public:
+      template<class PyClass>
+      void visit(PyClass & cl) const
+      {
+        cl.def(bp::init<int>(bp::args("self", "size"), "Default constructor."))
+
+          .def_readwrite("Kp", &Self::Kp, "Proportional corrector value.")
+          .def_readwrite("Kd", &Self::Kd, "Damping corrector value.")
+
+          .def(CastVisitor<Self>())
+          // .def(ExposeConstructorByCastVisitor<
+          //      Self, ::pinocchio::context::RigidConstraintModel::BaumgarteCorrectorParameters>())
+          .def(ComparableVisitor<Self, pinocchio::is_floating_point<Scalar>::value>());
+      }
+
+      static void expose()
+      {
+        // eigenpy::enableEigenPySpecific<VectorType>();
+        bp::class_<BaumgarteCorrectorParameters>(
+          "BaumgarteCorrectorParameters", "Paramaters of the Baumgarte Corrector.", bp::no_init)
+          .def(BaumgarteCorrectorParametersPythonVisitor());
+      }
+    };
+
+  } // namespace python
+} // namespace pinocchio
+
+#endif // ifndef __pinocchio_python_algorithm_constraints_baumgarte_corrector_parameters_hpp__
