293aa0a24d2db7d40fc48f62ae08a1d114bd252d
Yann de Mont-Marin <ydemontm@wifi-pro-82-134.paris.inria.fr>
2025-03-19 11:15:34 +0100

    Constraints-Set-Bindings: complete exposition of coulomb and box and factorize code following CRTP hierarchy

---
+++ include/pinocchio/bindings/python/algorithm/constraints/set-base.hpp
@@ -0,0 +1,55 @@
+//
+// Copyright (c) 2024 INRIA
+//
+
+#ifndef __pinocchio_python_algorithm_constraints_box_set_hpp__
+#define __pinocchio_python_algorithm_constraints_box_set_hpp__
+
+#include <eigenpy/eigenpy.hpp>
+
+#include "pinocchio/algorithm/constraints/box-set.hpp"
+
+#include "pinocchio/bindings/python/utils/cast.hpp"
+#include "pinocchio/bindings/python/utils/copyable.hpp"
+
+namespace pinocchio
+{
+  namespace python
+  {
+    namespace bp = boost::python;
+
+    template<typename Set, typename VectorLike>
+    struct SetPythonVisitor : public boost::python::def_visitor<SetPythonVisitor<Set>>
+    {
+      template<class PyClass>
+      void visit(PyClass & cl) const
+      {
+        cl.def(
+            "isInside", &Set::template isInside<VectorLike>, bp::args("self", "f"),
+            "Check whether a vector x lies within the cone.")
+          .def(
+            "project", &Set::template project<VectorLike>, bp::args("self", "f"),
+            "Normal projection of a vector f onto the cone.")
+          .def("dim", &Set::dim, "Returns the dimension of the cone.")
+          .def("size", &Set::size, "Returns the size of the cone.")
+#ifndef PINOCCHIO_PYTHON_SKIP_COMPARISON_OPERATIONS
+          .def(bp::self == bp::self)
+          .def(bp::self != bp::self)
+#endif
+          ;
+      }
+    };
+
+    template<typename ConeSet>
+    struct ConeSetPythonVisitor : public boost::python::def_visitor<ConeSetPythonVisitor<ConeSet>>
+    {
+      template<class PyClass>
+      void visit(PyClass & cl) const
+      {
+        cl.def("dual", &ConeSet::dual, bp::arg("self"), "Returns the dual cone associated to this");
+      }
+    };
+  } // namespace python
+} // namespace pinocchio
+
+#endif // ifndef __pinocchio_python_algorithm_constraints_box_set_hpp__
