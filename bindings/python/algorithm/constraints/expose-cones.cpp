21e63542b558ee59cebfd3bd082cd6701313ceb3
Yann de Mont-Marin <ydemontm@wifi-pro-82-134.paris.inria.fr>
2025-03-19 11:40:37 +0100

    Constraint: expose all sets - Update forward and exposition

--- bindings/python/algorithm/constraints/expose-cones.cpp
+++ bindings/python/algorithm/constraints/expose-cones.cpp
@@ -5,8 +5,11 @@
 #include "pinocchio/serialization/aligned-vector.hpp"
 
 #include "pinocchio/bindings/python/fwd.hpp"
-#include "pinocchio/bindings/python/algorithm/constraints/coulomb-friction-cone.hpp"
-#include "pinocchio/bindings/python/algorithm/constraints/box-set.hpp"
+#include "pinocchio/bindings/python/algorithm/constraints/set-coulomb-friction-cone.hpp"
+#include "pinocchio/bindings/python/algorithm/constraints/set-box-set.hpp"
+#include "pinocchio/bindings/python/algorithm/constraints/set-joint-limit-cone.hpp"
+#include "pinocchio/bindings/python/algorithm/constraints/set-trivial-cones.hpp"
+
 // #include "pinocchio/bindings/python/serialization/serialization.hpp"
 #include "pinocchio/bindings/python/utils/std-aligned-vector.hpp"
 
@@ -31,6 +34,14 @@ namespace pinocchio
       // #endif
 
       BoxSetPythonVisitor<context::BoxSet>::expose();
+      JointLimitConstraintConePythonVisitor<context::JointLimitConstraintCone>::expose();
+      TrivialConePythonVisitor<context::NullSet>::expose(
+        "NullSet", "Set reduce to 0 singleton in R^d.");
+      TrivialConePythonVisitor<context::UnboundedSet>::expose("UnboundedSet", "Set R^d.");
+      TrivialConePythonVisitor<context::PositiveOrthantCone>::expose(
+        "PositiveOrthantCone", "Set R_+^d.");
+      TrivialConePythonVisitor<context::NegativeOrthantCone>::expose(
+        "NegativeOrthantCone", "Set R_-^d.");
     }
   } // namespace python
 } // namespace pinocchio
