b154ab9abf1ed6c9b52ef4cc30a1899d4826acf5
Yann de Mont-Marin <ydemontm@wifi-pro-82-134.paris.inria.fr>
2025-04-01 17:59:55 +0200

    Create a visitor to expose liegroup of a joint

---
+++ include/pinocchio/bindings/python/multibody/joint/joints-liegroup.hpp
@@ -0,0 +1,46 @@
+//
+// Copyright (c) 2015-2021 CNRS INRIA
+//
+
+#ifndef __pinocchio_python_multibody_joint_joints_liegroup_hpp__
+#define __pinocchio_python_multibody_joint_joints_liegroup_hpp__
+
+#include <boost/python.hpp>
+
+#include "pinocchio/bindings/python/fwd.hpp"
+#include "pinocchio/multibody/liegroups.hpp"
+
+namespace pinocchio
+{
+  namespace python
+  {
+    namespace bp = boost::python;
+
+    template<class JointModelDerived>
+    struct JointModelLieGroupPythonVisitor
+    : public boost::python::def_visitor<JointModelLieGroupPythonVisitor<JointModelDerived>>
+    {
+    public:
+      typedef CartesianProductOperationVariantTpl<
+        context::Scalar,
+        context::Options,
+        LieGroupCollectionDefaultTpl>
+        LieGroupOperation;
+      typedef typename LieGroupMap::operation<JointModelDerived>::type LieGroupType;
+
+      template<class PyClass>
+      void visit(PyClass & cl) const
+      {
+        cl.def("liegroup", &liegroup).staticmethod("liegroup");
+      }
+
+      static LieGroupOperation liegroup()
+      {
+        return LieGroupOperation(LieGroupType());
+      }
+    };
+
+  } // namespace python
+} // namespace pinocchio
+
+#endif // ifndef __pinocchio_python_multibody_joint_joints_liegroup_hpp__
