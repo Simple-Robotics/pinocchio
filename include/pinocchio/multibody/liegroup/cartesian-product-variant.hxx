7dd8367c9d98952ea80e10ba4c2fca99b12c6977
Yann de Mont-Marin <ydemontm@wifi-pro-82-134.paris.inria.fr>
2025-04-09 15:03:50 +0200

    LieGroup: specialize tangentmap for cartesian variant

--- include/pinocchio/multibody/liegroup/cartesian-product-variant.hxx
+++ include/pinocchio/multibody/liegroup/cartesian-product-variant.hxx
@@ -232,6 +232,69 @@ namespace pinocchio
   }
 
   template<typename _Scalar, int _Options, template<typename, int> class LieGroupCollectionTpl>
+  template<class Config_t, class TangentMap_t>
+  void
+  CartesianProductOperationVariantTpl<_Scalar, _Options, LieGroupCollectionTpl>::tangentMap_impl(
+    const Eigen::MatrixBase<Config_t> & q,
+    Eigen::MatrixBase<TangentMap_t> & TM,
+    const AssignmentOperatorType op) const
+  {
+    if (op == SETTO)
+      TM.setZero();
+    Index id_q = 0, id_v = 0;
+    for (size_t k = 0; k < liegroups.size(); ++k)
+    {
+      const Index & nq = lg_nqs[k];
+      const Index & nv = lg_nvs[k];
+      ::pinocchio::tangentMap(liegroups[k], q.segment(id_q, nq), TM.block(id_q, id_v, nq, nv), op);
+      id_q += nq;
+      id_v += nv;
+    }
+  }
+
+  template<typename _Scalar, int _Options, template<typename, int> class LieGroupCollectionTpl>
+  template<class Config_t, class MatrixIn_t, class MatrixOut_t>
+  void CartesianProductOperationVariantTpl<_Scalar, _Options, LieGroupCollectionTpl>::
+    tangentMapProduct_impl(
+      const Eigen::MatrixBase<Config_t> & q,
+      const Eigen::MatrixBase<MatrixIn_t> & Min,
+      Eigen::MatrixBase<MatrixOut_t> & Mout,
+      const AssignmentOperatorType op) const
+  {
+    Index id_q = 0, id_v = 0;
+    for (size_t k = 0; k < liegroups.size(); ++k)
+    {
+      const Index & nq = lg_nqs[k];
+      const Index & nv = lg_nvs[k];
+      ::pinocchio::tangentMapProduct(
+        liegroups[k], q.segment(id_q, nq), Min.middleRows(id_v, nv), Mout.middleRows(id_q, nq), op);
+      id_q += nq;
+      id_v += nv;
+    }
+  }
+
+  template<typename _Scalar, int _Options, template<typename, int> class LieGroupCollectionTpl>
+  template<class Config_t, class MatrixIn_t, class MatrixOut_t>
+  void CartesianProductOperationVariantTpl<_Scalar, _Options, LieGroupCollectionTpl>::
+    coTangentMapProduct_impl(
+      const Eigen::MatrixBase<Config_t> & q,
+      const Eigen::MatrixBase<MatrixIn_t> & Min,
+      Eigen::MatrixBase<MatrixOut_t> & Mout,
+      const AssignmentOperatorType op) const
+  {
+    Index id_q = 0, id_v = 0;
+    for (size_t k = 0; k < liegroups.size(); ++k)
+    {
+      const Index & nq = lg_nqs[k];
+      const Index & nv = lg_nvs[k];
+      ::pinocchio::coTangentMapProduct(
+        liegroups[k], q.segment(id_q, nq), Min.middleRows(id_q, nq), Mout.middleRows(id_v, nv), op);
+      id_q += nq;
+      id_v += nv;
+    }
+  }
+
+  template<typename _Scalar, int _Options, template<typename, int> class LieGroupCollectionTpl>
   template<class Config_t, class Tangent_t, class JacobianIn_t, class JacobianOut_t>
   void CartesianProductOperationVariantTpl<_Scalar, _Options, LieGroupCollectionTpl>::
     dIntegrateTransport_dq_impl(
