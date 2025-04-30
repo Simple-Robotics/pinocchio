29b889d09a2dcfcadf556e9b86ec913da49a9afa
Justin Carpentier <justin.carpentier@inria.fr>
2025-04-30 15:24:27 +0200

    algo/constraints: fix guard

--- include/pinocchio/algorithm/constraints/binary-constraint-base.hpp
+++ include/pinocchio/algorithm/constraints/binary-constraint-base.hpp
@@ -2,8 +2,8 @@
 // Copyright (c) 2023-2025 INRIA
 //
 
-#ifndef __pinocchio_algorithm_constraints_unary_constraint_base_hpp__
-#define __pinocchio_algorithm_constraints_unary_constraint_base_hpp__
+#ifndef __pinocchio_algorithm_constraints_binary_constraint_base_hpp__
+#define __pinocchio_algorithm_constraints_binary_constraint_base_hpp__
 
 #include "pinocchio/algorithm/fwd.hpp"
 #include "pinocchio/algorithm/constraints/constraint-model-base.hpp"
@@ -67,4 +67,4 @@ namespace pinocchio
   }; // struct BinaryConstraintModelBase
 } // namespace pinocchio
 
-#endif // ifndef __pinocchio_algorithm_constraints_unary_constraint_base_hpp__
+#endif // ifndef __pinocchio_algorithm_constraints_binary_constraint_base_hpp__
