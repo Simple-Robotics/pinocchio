7758a11d1269e60c824d42a4c508e61776ca0de3
Justin Carpentier <justin.carpentier@inria.fr>
2025-05-15 16:05:22 +0200

    math/linalg: enhance efficiency

--- include/pinocchio/math/details/matrix-inverse-1x1.hpp
+++ include/pinocchio/math/details/matrix-inverse-1x1.hpp
@@ -30,7 +30,7 @@ namespace pinocchio
         a0 = input_vec[0];
         a0 = math::sqrt(a0);
         a1 = (Scalar(1) / a0);
-        a1 = (a1 / a0);
+        a1 = math::square(a1);
         output_vec[0] = a1;
       }
     };
