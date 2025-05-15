b4870bea6be7c74723c2f72f9a2b91ef75c709fd
Justin Carpentier <justin.carpentier@inria.fr>
2025-05-15 10:58:35 +0200

    test/math: test first version of code generated inversion

---
+++ unittest/matrix-inverse.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2025 INRIA
+//
+
+#include <iostream>
+
+#include <pinocchio/math/matrix-inverse.hpp>
+
+#include <boost/variant.hpp> // to avoid C99 warnings
+
+#include <boost/test/unit_test.hpp>
+#include <boost/utility/binary.hpp>
+
+BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
+
+using namespace pinocchio;
+
+template<int size>
+using MatrixTpl = Eigen::Matrix<double, size, size>;
+
+BOOST_AUTO_TEST_CASE(test_generated_inverse)
+{
+  typedef MatrixTpl<4> Matrix;
+  Matrix mat = Matrix::Random();
+  mat = mat.transpose() * mat;
+  make_symmetric(mat);
+  BOOST_CHECK(is_symmetric(mat, 0));
+
+  Matrix res = Matrix::Zero();
+  matrix_inversion_code_generated(mat, res);
+  BOOST_CHECK((res * mat).isIdentity());
+  BOOST_CHECK(mat.inverse().isApprox(res));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
